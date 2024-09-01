6453278ec730ae5c37a829617d7546d5cfea153f
CyberKL <lotfyk29@yahoo.com>
2024-09-01 23:33:40 +0300

    Improve performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
 import AppRoutes from "./routes/Routes";
 import Layout from "./Layout";
-import { useSelector } from "react-redux";
-import { useEffect } from "react";
 
 function App() {
-  const theme = useSelector((state) => state.theme);
-
-  useEffect(() => {
-    document.querySelector("html").classList.remove("light", "dark");
-    document.querySelector("html").classList.add(theme);
-  }, [theme]);
-
   return (
     <Layout>
       <AppRoutes />
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ThemeToggleButton from "./components/common/ThemeToggleButton";
+import { useSelector } from "react-redux";
 
 const Layout = ({ children }) => {
+  const theme = useSelector((state) => state.theme);
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(theme);
+  }, [theme]);
+  
   return (
     <div>
       <div className="fixed bottom-4 right-4 z-50">
